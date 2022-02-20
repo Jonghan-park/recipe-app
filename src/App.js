7902c1834aad2ab39e2ff835a42ed8a8fa653f67
jonghan-park <jonghan.park@edu.sait.ca>
2022-02-19 23:16:51 -0700

    Update api id and key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () =>{
+
+  const APP_ID = 'f9cd1351'; 
+  const APP_KEY = 'bcdc50153437d299ec8598d68f97cea9';
+
+  const exampleReq = 
+  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
