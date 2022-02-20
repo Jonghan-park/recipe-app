a9fb03ad7f9d06fdc222a8c71a1f4e90f57e6aef
jonghan-park <jonghan.park@edu.sait.ca>
2022-02-19 23:47:59 -0700

    Make some recipes on the screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useEffect, useState} from "react";
-import logo from './logo.svg';
+import Recipe from "./Recipe";
 import './App.css';
 
 const App = () =>{
@@ -17,6 +17,7 @@ const App = () =>{
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits);
   }
 
   return (
@@ -25,6 +26,12 @@ const App = () =>{
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe 
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image} />
+      ))};
     </div>
   );
 }
